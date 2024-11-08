d58bc98f62fe555f0624cbba72a4071516ddcc08
kinya-njui <kinyanjuicharlesm@gmail.com>
2024-11-08 19:11:24 +0300

    creating a route to fetch single blog using id

--- Documents/TEACH2GIVE PROJECTS/Full Stack/BlogIt Platform/server/src/controllers/blogs.controllers.js
+++ Documents/TEACH2GIVE PROJECTS/Full Stack/BlogIt Platform/server/src/controllers/blogs.controllers.js
@@ -40,6 +40,25 @@ async function getAllBlogs(_req, res) {
     res.status(500).json({ message: error.message });
     return;
   }
+};
+
+//fetching single blog using id
+async function getSingleBlog(req, res) {
+  try {
+    const { id } = req.params;
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!blog) {
+      res.status(404).json({ message: "Blog not found" });
+      return;
+    }
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-export { createBlog, getAllBlogs };
+export { createBlog, getAllBlogs, getSingleBlog };
--- Documents/TEACH2GIVE PROJECTS/Full Stack/BlogIt Platform/server/src/index.js
+++ Documents/TEACH2GIVE PROJECTS/Full Stack/BlogIt Platform/server/src/index.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import signupUser from "./controllers/Auth/signupUser.js";
 import loginUser from "./controllers/Auth/loginUsers.js";
 import validateUserInformation from "./middleware/validateUserInformation.js";
-import { createBlog, getAllBlogs } from "./controllers/blogs.controllers.js";
+import { createBlog, getAllBlogs, getSingleBlog } from "./controllers/blogs.controllers.js";
 import verifyToken from "./middleware/verifyToken.js";
 import ValidateBlog from "./middleware/validateBlog.js";
 const app = express();
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.post("/signup", validateUserInformation, signupUser);
 app.post("/auth/login", loginUser);
 app.post("/blog", verifyToken, ValidateBlog, createBlog);
+app.get("/blog/:id", verifyToken, getSingleBlog);
 
 // route for getting all blog posts
 app.get("/all-blogs", verifyToken, getAllBlogs);
